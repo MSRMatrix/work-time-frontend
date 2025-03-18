548d17ddf3cc95e70d4fccee4f5ea4dc5d9352f3
TwinMine <michaelrolo11@gmail.com>
2025-03-18 09:26:45 +0100

    deploy test

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,15 +1,43 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Header from "../header/Header";
 import { useContext, useEffect } from "react";
 import { Time, User } from "../context/Context";
-import { getData } from "../functions/getData";
 
 const Home = () => {
   const { time, setTime } = useContext(Time);
   const { user, setUser } = useContext(User);
+  const navigate = useNavigate();
+
+   async function getData(setTime, setUser) {
+    const URL = import.meta.env.VITE_BACKENDURL;
+    try {
+      const response = await fetch(`${URL}/user`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        navigate("/")
+        return console.log(response.statusText);
+      } else {
+        setTime(data.timelog);
+        setUser(data.user);
+        navigate("/dashboard")
+        console.log(`test`);
+        
+        return console.log("Daten erfolgreich geladen!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
 
   useEffect(() => {
-    getData(setTime, setUser);
+    getData(setTime, setUser)
   }, []);
 
   return (
